06e32f8046dc86ceea6f83f548e2a1f5fed0c1e8
Kcrb <corcoding@gmail.com>
2021-11-23 01:37:19 -0500

    Clean up main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-/* Straight JavaScript */
+window.onload = function () {
+	const goToLinks = document.querySelectorAll('.go-to-links > a');
 
-window.onload = function() {
-
-
-
-}
-
-/* JQuery */
-
-$(document).ready(function() {
-
-
-
-});
+	goToLinks.forEach(goLink => {
+		goLink.addEventListener('click', (e) => {
+			e.preventDefault();
+			let sectionClass = e.target.href.split('#');
+			document.querySelector(`#${sectionClass[1]}`).scrollIntoView({ 
+				behavior: 'smooth' 
+			});
+		});
+	});
+};
